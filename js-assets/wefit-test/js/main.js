b146b164d2dfa38576e59c7b657ad49da3fa42cf
Vinicius <viniciusiandrade@hotmail.com>
2024-03-28 18:38:59 -0300

    feat: addItemsOnList function

--- js-assets/wefit-test/js/main.js
+++ js-assets/wefit-test/js/main.js
@@ -16,3 +16,23 @@ const moveContentToRight = () => {
     button.classList.add('bg-success', 'border-0');
 };
 moveContentToRight();
+
+// Itens
+const addItemsOnList = () => {
+    const list = document.querySelector('.list-group');
+    const fourthItem = document.createElement('li');
+    const fifthItem = document.createElement('li');
+
+    list.firstElementChild.classList.remove('active');
+    
+    fourthItem.classList.add('list-group-item', 'active');
+    fifthItem.classList.add('list-group-item');
+
+    fourthItem.innerHTML = 'Quarto item';
+    fifthItem.innerHTML = 'Quinto item';
+
+    list.appendChild(fourthItem)
+    list.appendChild(fifthItem)
+};
+addItemsOnList();
+
