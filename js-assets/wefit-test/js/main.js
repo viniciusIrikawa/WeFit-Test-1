e0df32c96a49b41b8d38263273055165b84774ed
Vinicius <viniciusiandrade@hotmail.com>
2024-03-28 15:27:04 -0300

    header manipulation

--- js-assets/wefit-test/js/main.js
+++ js-assets/wefit-test/js/main.js
@@ -6,3 +6,13 @@ const transformIntoInlineMenu = () => {
 };
 
 transformIntoInlineMenu();
+
+// Header
+const moveContentToRight = () => {
+    const jumbotron = document.querySelector('.jumbotron');
+    const button = jumbotron.querySelector('.btn-primary'); 
+
+    jumbotron.classList.add('bg-secondary', 'text-white', 'd-flex', 'justify-content-end', 'flex-column', 'align-items-end');
+    button.classList.add('bg-success', 'border-0');
+};
+moveContentToRight();
